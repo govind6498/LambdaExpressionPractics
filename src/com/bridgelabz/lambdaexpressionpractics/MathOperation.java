27da063134443d2c17390313fbf871b1f1eb71e1
govind6498 <govindsingh6498@gmail.com>
2021-09-29 12:56:08 +0530

    Added:TO Create a playlist using Lambda function

--- src/com/bridgelabz/lambdaexpressionpractics/MathOperation.java
+++ src/com/bridgelabz/lambdaexpressionpractics/MathOperation.java
@@ -3,7 +3,7 @@ package com.bridgelabz.lambdaexpressionpractics;
 interface IMathFunction{
 	int calculate(int a,int b);
 	static void printResult(int a,int b,String function,IMathFunction fobj) {
-		System.out.println("Result of "+function+" is "+fobj.calculate(a,b));
+		System.out.println("Result of "+function+" is "+fobj.calculate(a, b));
 	}
 }
 public class MathOperation {
@@ -12,6 +12,12 @@ public class MathOperation {
 		IMathFunction sub = (x,y)->x-y;
 		IMathFunction mul = (x,y)->x*y;
 		IMathFunction div = (x,y)->x/y;
+		
+//		System.out.println("Addition is:"+add.calculate(5,8));
+//		System.out.println("Subtration is:"+sub.calculate(5,8));
+//		System.out.println("Multiplication is:"+mul.calculate(5,8));
+//		System.out.println("Division is:"+div.calculate(5,8));
+		
 		
 		IMathFunction.printResult(6,3,"Addition",add);
 		IMathFunction.printResult(6,3,"Subtratcion",sub);
---
+++ src/com/bridgelabz/lambdaexpressionpractics/NumberPlayList.java
@@ -0,0 +1,51 @@
+package com.bridgelabz.lambdaexpressionpractics;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class NumberPlayList {
+	public static void main(String[] args) {
+		//Creating sample collection
+		List<Integer> myNumberList = new ArrayList<Integer>();
+		for(int i=0;i<5;i++) {
+			System.out.println(i);
+		}
+
+		//Method1:traversing using Iterator
+		Iterator<Integer> it= myNumberList.iterator();
+		while(it.hasNext()) {
+			Integer i=it.next();
+			System.out.println("Mth1:Iterator value:"+i);
+		}
+		
+		//Method2:Traversing with Explicit Consumer interface Implementation
+		class myConsumer implements Consumer<Integer>{
+			public void accept(Integer t) {
+				System.out.println("Mth2:Consumer imple value::"+t);
+			}
+		}
+		myConsumer action = new myConsumer();
+		myNumberList.forEach(action);
+		
+		//Method3:Traversing with Anonymous Consumer interface implementation
+		myNumberList.forEach(new Consumer<Integer>() {
+			public void accept(Integer t) {
+				System.out.println("Mth3:ForEach anonymous class value:"+t);
+			}
+		});
+		
+		//Method4:Explicit Lambda function
+		Consumer<Integer> myListAction =n->{
+			System.out.println("Mth4:foreach Lambda impl value::"+n);
+
+		}
+		myNumberList.forEach(myListAction);
+
+		//Method 5:Implicit lambda function
+		myNumberList.forEach(n->{
+			System.out.println("Mth5:forEach Lambda impl value:"+n);
+		});
+	}
+}
