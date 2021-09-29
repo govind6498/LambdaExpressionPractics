5901f4eecb1ef5df0e72766a6de13b56b898274a
govind6498 <govindsingh6498@gmail.com>
2021-09-29 12:05:07 +0530

    Added:Ability to show result of Math Opeartion to pass Lambda Function

--- src/com/bridgelabz/lambdaexpressionpractics/MathOperation.java
+++ src/com/bridgelabz/lambdaexpressionpractics/MathOperation.java
@@ -2,6 +2,9 @@ package com.bridgelabz.lambdaexpressionpractics;
 @FunctionalInterface
 interface IMathFunction{
 	int calculate(int a,int b);
+	static void printResult(int a,int b,String function,IMathFunction fobj) {
+		System.out.println("Result of "+function+" is "+fobj.calculate(a,b));
+	}
 }
 public class MathOperation {
 	public static void main(String[] args) {
@@ -10,9 +13,11 @@ public class MathOperation {
 		IMathFunction mul = (x,y)->x*y;
 		IMathFunction div = (x,y)->x/y;
 		
-		System.out.println("Addition is:"+add.calculate(5,8));
-		System.out.println("Subtration is:"+sub.calculate(5,8));
-		System.out.println("Multiplication is:"+mul.calculate(5,8));
-		System.out.println("Division is:"+div.calculate(5,8));
+		IMathFunction.printResult(6,3,"Addition",add);
+		IMathFunction.printResult(6,3,"Subtratcion",sub);
+		IMathFunction.printResult(6,3,"Multiplication",mul);
+		IMathFunction.printResult(6,3,"Division",div);
+		
+		
 	}
 }
