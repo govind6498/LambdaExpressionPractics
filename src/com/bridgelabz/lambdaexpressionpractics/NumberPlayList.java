7f54d84cbb6f82077d4b0db64596e0ac2a6a74c2
govind6498 <govindsingh6498@gmail.com>
2021-09-29 13:10:53 +0530

    Added:TO Print into Double value

--- src/com/bridgelabz/lambdaexpressionpractics/NumberPlayList.java
+++ src/com/bridgelabz/lambdaexpressionpractics/NumberPlayList.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class NumberPlayList {
 	public static void main(String[] args) {
@@ -19,7 +20,7 @@ public class NumberPlayList {
 			Integer i=it.next();
 			System.out.println("Mth1:Iterator value:"+i);
 		}
-		
+
 		//Method2:Traversing with Explicit Consumer interface Implementation
 		class myConsumer implements Consumer<Integer>{
 			public void accept(Integer t) {
@@ -28,24 +29,30 @@ public class NumberPlayList {
 		}
 		myConsumer action = new myConsumer();
 		myNumberList.forEach(action);
-		
+
 		//Method3:Traversing with Anonymous Consumer interface implementation
 		myNumberList.forEach(new Consumer<Integer>() {
 			public void accept(Integer t) {
 				System.out.println("Mth3:ForEach anonymous class value:"+t);
 			}
 		});
-		
-		//Method4:Explicit Lambda function
-		Consumer<Integer> myListAction =n->{
-			System.out.println("Mth4:foreach Lambda impl value::"+n);
 
-		}
-		myNumberList.forEach(myListAction);
+		//Method4:Explicit Lambda function
+		//		Consumer<Integer> myListAction =n->{
+		//			System.out.println("Mth4:foreach Lambda impl value::"+n);
+		//
+		//		}
+		//		myNumberList.forEach(myListAction);
 
 		//Method 5:Implicit lambda function
 		myNumberList.forEach(n->{
 			System.out.println("Mth5:forEach Lambda impl value:"+n);
+		});
+
+		//Method 6:Implicit Lambda function to print double value
+		Function<Integer,Double>toDoubleFunction =n-> n.doubleValue();
+		myNumberList.forEach(n->{
+			System.out.println("Mth6:foreach Lambda double value::"+toDoubleFunction.apply(n));	
 		});
 	}
 }
